a8dd5889875afe2646f0cb6d370b13e9bd7c0e98
agaraujo25 <agaraujo@grupoasesores.com.mx>
2021-03-14 22:29:54 -0600

    solved bug

--- src/pages/references.js
+++ src/pages/references.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../components/header'
 import Footer from '../components/footer'
-import List from '../components/List'
+import List from '../components/list'
 
 export default function References() {
     return (
